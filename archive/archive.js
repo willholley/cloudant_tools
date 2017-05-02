a84bfe0a61101dad9346843b223cc018728bea80
Will Holley <willholley@gmail.com>
2017-05-02 11:10:17 +0100

    add mediaskunkworks archive script

--- archive/archive.js
+++ archive/archive.js
@@ -1,30 +1,160 @@
-var request = require('request');
+var PouchDB = require('pouchdb'),
+	crypto = require('crypto'),
+	shasum = crypto.createHash('sha1'),
+	winston = require('winston'),
+	config = require('config');
 
-var SOURCE = 'https://willholley.cloudant.com/sourcetest';
-var TARGET = 'https://willholley.cloudant.com/targettest';
+winston.level = process.env.LOG_LEVEL;
 
-var BATCH_SIZE = 500;
-var SELECTOR = '';
-var START_SEQ=0;
+winston.log('info', 'starting archive');
 
-// while no more changes
+var SOURCE = config.get('source');
+var TARGET = config.get('target');
+var SELECTOR = config.get('selector');
+var BATCH_SIZE = config.get('batch_size');
+var START_SEQ = config.get('start_seq');
+var DRY_RUN = config.get('dry_run');
 
-// read batch_size docs from _changes, with selector
-// POST _changes?filter=_selector&include_docs=true
-// {"selector": SELECTOR}
+if (SOURCE.length === 0) {
+	throw Error('no source database defined');
+}
 
-// for each doc
-// POST batch to target
-// check all succeed
-// POST delete to source for each
+if (TARGET.length === 0) {
+	throw Error('no target database defined');
+}
+
+if (!DRY_RUN) {
+	winston.log('info', 'NOT DRY RUN!!!');
+} else {
+	winston.log('info', 'DRY RUN');
+}
+
+var source = new PouchDB(SOURCE);
+var target = new PouchDB(TARGET);
+
+var count = 0;
+
+var checkpointDoc = '_local/' + shasum.update(SOURCE).digest('hex');
 
 
-// track progress/output, last seq processed
-var changesUrl = SOURCE + '_changes?filter=_selector&include_docs=true';
+function writeCheckpoint (seq) {
+	getCheckpoint().then(function (doc) {
+		winston.log('verbose', 'writing checkpoint ' + checkpointDoc);
+		doc.seq = seq;
+		return target.put(doc);
+	});
+}
 
-while(true) {
-	request.post(changesUrl)
+function getCheckpoint () {
+	winston.log('verbose', 'fetching checkpoint ' + checkpointDoc);
+	return target.get(checkpointDoc).
+		catch(function (err) {
+			winston.log('verbose', 'checkpoint ' + checkpointDoc, err);
+
+			if (err.status === 404) {
+				return {
+					'_id': checkpointDoc,
+					'seq': 0
+				};
+			}
+			winston.log('error', err);
+		}).then(function (doc) {
+			return doc;
+		});
+}
+
+function writeBatch (seq, changes) {
+	winston.log('verbose', 'writeBatch', changes);
+	var lastSeq = changes.last_seq;
+	var docs = changes.results.map(row => row.doc);
+
+	if (DRY_RUN) {
+		// non-destructive run
+		count = count + docs.length;
+		winston.log('info', 'DRY RUN: total docs moved: ' + count);
+
+		getNextBatch(lastSeq);
+		return;
+	}
+
+	target.bulkDocs(docs, {
+		new_edits: false
+	}).then(function (result) {
+		winston.log('info', 'source sequence ' + lastSeq + ' written to target');
+
+		deleteBatch(seq, lastSeq, docs);
+	}).catch(function (err) {
+		winston.log('error', 'FATAL: error writing batch for seq ' + seq + ':\n' + err);
+	});
+}
+
+function deleteBatch (startSeq, lastSeq, docs) {
+	winston.log('verbose', 'deleteBatch', {
+		"startSeq": startSeq,
+		"lastSeq": lastSeq,
+		"docs": docs
+	});
+
+	var idRevPairs = docs.map(function(doc) {
+		return {
+			_id: doc._id,
+			_rev: doc._rev,
+			_deleted: true
+		};
+	});
+	source.bulkDocs(idRevPairs).then(function (result) {
+		count = count + idRevPairs.length;
+		winston.log('info', 'source sequence ' + lastSeq + 'deleted from source');
+		winston.log('info', 'total docs moved: ' + count);
+
+		getNextBatch(lastSeq);
+	}).catch(function (err) {
+		winston.log('warn', 'error writing batch for start seq ' + startSeq + ':\n' + err);
+
+		// ignore failed deletions for now
+		getNextBatch(lastSeq);
+	});
 }
 
 
+function getNextBatch(seq) {
+	winston.log('verbose', 'getNextBatch', {
+		"seq": seq,
+		"selector": JSON.stringify(SELECTOR)
+	});
 
+	source.changes({
+		include_docs: true,
+		since: seq,
+		limit: BATCH_SIZE,
+		selector: SELECTOR
+	}).on('complete', function(info) {
+		if (info.results.length === 0) {
+			winston.log('info', 'No more results at seq ' + seq);
+			winston.log('info', 'All done!');
+			return;
+		}
+
+		try {
+			writeBatch(seq, info);
+		} catch (e) {
+			console.log(e);
+		}
+
+	}).on('error', function(err) {
+		winston.log('error', err);
+	});
+}
+
+if (START_SEQ === 0) {
+	winston.log('verbose', 'no start seq provided');
+
+	getCheckpoint().then(function (doc) {
+		winston.log('verbose', 'using start_seq ' + doc.seq);
+		getNextBatch(doc.seq);
+	});
+}
+else {
+	winston.log('verbose', 'using start_seq ' + START_SEQ);
+	getNextBatch(START_SEQ);
+}
